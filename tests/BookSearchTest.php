7e78b0a7d3b13d26a2b75c4954f3ef8c7eca9b86
Muhammad Dimas R. <draxozy@gmail.com>
2025-04-07 19:53:20 +0800

    Update BookSearchTest.php

--- tests/BookSearchTest.php
+++ tests/BookSearchTest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 class BookSearchTest extends TestCase {
     public function testSearchBooks() {
-        require 'books-api_books.php';
+        require 'books-api.php';
         $result = searchBooks("PHP");
         $this->assertArrayHasKey('items', $result);
     }
