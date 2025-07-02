9d00bec33544e5c244f8f3adbe7b9ec3b7a959f8
husnulkhamaisyah <husnulkhamaisyah@gmail.com>
2025-07-02 21:59:14 +0800

    menambahkan file string_helper.php berisi fungsi slugify, truncate, random string, dll

---
+++ string_helper.php
@@ -0,0 +1,23 @@
+<?php
+// string_helper.php
+
+function slugify($text) {
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    $text = trim($text, '-');
+    $text = preg_replace('~-+~', '-', $text);
+    return strtolower($text);
+}
+
+function capitalizeWords($text) {
+    return ucwords(strtolower($text));
+}
+
+function truncateText($text, $max = 100) {
+    return strlen($text) > $max ? substr($text, 0, $max) . '...' : $text;
+}
+
+function randomString($length = 10) {
+    return substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, $length);
+}
