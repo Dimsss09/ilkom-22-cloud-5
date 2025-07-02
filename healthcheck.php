23a2ca042ff4e70cf6a5361a34e8e063ce309489
Dhany R <daniramadandaniramadan@gmail.com>
2025-07-03 03:24:59 +0800

    feat : mengecek koneksi databasee di file healthcheck.php.

--- healthcheck.php
+++ healthcheck.php
@@ -4,3 +4,12 @@
 require_once 'function.php'; // memastikan koneksi $conn tersedia
 
 header('Content-Type: application/json');
+
+// Cek koneksi database
+if (!$conn) {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Koneksi database gagal'
+    ]);
+    exit;
+}
