edd67cc196da4c53b7fd50e6f56212350fe2b189
Dhany R <daniramadandaniramadan@gmail.com>
2025-07-03 03:14:58 +0800

    feat: menambahkan data dummy data user, bisa juga menggunakan data dari DB pada file profile.php

--- profile.php
+++ profile.php
@@ -6,3 +6,8 @@ if (!isset($_SESSION['login'])) {
     header("Location: login.php");
     exit();
 }
+
+// Dummy data pengguna (bisa diganti dari DB)
+$username = "DhanyR";
+$email = "dhany@kece.com";
+$role = "Administrator";
