5f823e18d0c26607ae417ac6ad43adceefa50273
Dhany R <daniramadandaniramadan@gmail.com>
2025-07-03 02:59:43 +0800

    feat: validasi login dan ambil nama user

--- welcome.php
+++ welcome.php
@@ -3,3 +3,11 @@
 
 session_start();
 require_once 'function.php';
+
+// Cek apakah user sudah login
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+
+$username = $_SESSION['username'];
