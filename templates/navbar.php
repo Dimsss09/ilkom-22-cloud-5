8436ed671c7a08baaa11f553a220efbd45b6ee6a
Putriiwlndr <putriekawulandari04@gmail.com>
2025-07-02 19:45:11 +0800

    menambahkan komentar pada file navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,15 +1,34 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <!-- Navbar Brand-->
+    <!-- Navigasi bagian atas dengan tema gelap -->
+
+    <!-- Navbar Brand -->
     <a class="navbar-brand ps-3" href="index.php">E-Library BRIDA</a>
-    <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-    <!-- Navbar-->
+    <!-- Teks brand yang mengarah ke halaman utama (index.php) -->
+
+    <!-- Sidebar Toggle -->
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
+        <i class="fas fa-bars"></i>
+    </button>
+    <!-- Tombol untuk toggle sidebar, menggunakan ikon hamburger -->
+
+    <!-- Navbar kanan -->
     <ul class="navbar-nav ms-auto me-3">
+        <!-- Navigasi item disusun ke kanan dengan margin kiri dan kanan -->
+
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <!-- Dropdown menu untuk user -->
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fas fa-user fa-fw"></i>
+            </a>
+            <!-- Ikon user dengan toggle dropdown -->
+
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                <!-- Menu dropdown muncul di sisi kanan -->
+                <li>
+                    <a class="dropdown-item" href="logout.php">Logout</a>
+                    <!-- Opsi logout -->
+                </li>
             </ul>
         </li>
     </ul>
-</nav> 
+</nav>
